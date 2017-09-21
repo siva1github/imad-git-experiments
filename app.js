e0a006614764d6e1902a1d0f78535424bafc123f
siva1github <gsrkpr@gmail.com>
2017-09-21 15:17:34 -0700

    Halfway done with new feature, so far so good

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+function aBigNewFeature(){
+  console.log('Writing a lot of code.');
+  console.log('Halfway done with this new feature');
+}
 function something(){
 	console.log('I\'m something');
 }
 console.log('Hello World');
 something();
+aBigNewFeature();
