61f17d44163e8fa82416e47068d215e12d91defd
siva1github <gsrkpr@gmail.com>
2017-09-23 20:35:30 -0700

    new-feature completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function aBigNewFeature(){
   console.log('Writing a lot of code.');
   console.log('Halfway done with this new feature');
+  console.log('Finally completely done with this new feature');
 }
 function something(){
 	console.log('I\'m something');
